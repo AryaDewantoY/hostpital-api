76c7b1b9a54919d7c941bed7d430e6f9d73b1c06
Arya Dewanto <aryayd@gmail.com>
2025-05-11 20:22:52 +0700

    refs

--- config/config.go
+++ config/config.go
@@ -14,17 +14,15 @@ var Secret = []byte("mysecret")
 
 func Connect() {
 	dsn := os.Getenv("DATABASE_URL")
-	var err error // Hanya mendeklarasikan err sekali
+	var err error
 
-	// Membuka koneksi ke database
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("failed to connect database", err) // Menggunakan err yang sudah dideklarasikan
+		log.Fatal("failed to connect database", err)
 	}
 
-	// AutoMigrate semua model
 	err = DB.AutoMigrate(&models.User{}, &models.Patient{}, &models.Doctor{}, &models.Appointment{})
 	if err != nil {
-		log.Fatal("failed to migrate models", err) // Menggunakan err yang sama
+		log.Fatal("failed to migrate models", err)
 	}
 }
